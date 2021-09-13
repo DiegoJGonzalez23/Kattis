a7a89599b14d65918aca73cb66467525aa61d1c5
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2021-09-13 09:31:07 -0400

    Add files via upload

---
+++ FlyingSafely.java
@@ -0,0 +1,37 @@
+//package ss;
+import java.util.*;
+
+// Diego Gonzalez
+// 9.13.2021
+// Problem flyingsafely
+public class FlyingSafely {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = sc.nextInt();
+		
+		for(int x = 0; x < tc;x++){
+			
+			int cities = sc.nextInt();
+			int pilots = sc.nextInt();
+			for(int p = 0; p < pilots; p++){
+				int j = sc.nextInt();
+				int i = sc.nextInt();
+			}
+			
+			
+			System.out.println(cities-1);
+			
+			
+			
+			
+			
+			
+		}
+		
+		sc.close();
+
+	}
+
+}
