262b41d8a90541ad5fb5886131abe891fe097751
Diego Gonzalez <51039803+DiegoJGonzalez23@users.noreply.github.com>
2022-02-19 13:32:03 -0500

    Create stuckinatimeloop.cpp

---
+++ stuckinatimeloop.cpp
@@ -0,0 +1,20 @@
+// Diego Gonzalez code 12.25.21
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int times;
+
+    cin >> times;
+
+    for (int i = 0; i < times; i++) {
+
+        cout << (i + 1) << " Abracadabra" << endl;
+
+
+    }
+
+
+}
+
